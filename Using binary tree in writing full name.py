dacfa52c9cce439bd2271f154023164dde4405f1
domssab <saballo.chantaldominique@gmail.com>
2023-01-14 23:29:43 +0800

    def buildtree

--- Using binary tree in writing full name.py
+++ Using binary tree in writing full name.py
@@ -22,9 +22,27 @@ class BinarySearchTreeNode:
                 self.right = BinarySearchTreeNode(data)
 
     def in_order_traversal(self):
-        elements - []
-
+        elements = []
+        # visit left tree
         if self.left:
             elements += self.left.in_order_traversal()
 
+        # visit base node
+        elements.append(self.data)
+
+        # visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
         return elements
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range (1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    letters = [C, H, A, N, T, A, L, D, O, M, I, N, Q, U, E, C, S, A, B, A, L, L, O]
